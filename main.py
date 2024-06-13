f6c47692cfef396a4d2c4b3e1493ef262af5a467
evadshell <evadshell69@gmail.com>
2024-06-13 17:26:35 +0530

    yt tut

--- main.py
+++ main.py
@@ -2,15 +2,45 @@
 from langchain_community.llms import Ollama
 from crewai import Agent,Task,Crew,Process
 
-model = Ollama(model="llama3:8b")
+model = Ollama(model="llama3")
 
 # code = "console.log(`hello world`)"
 
-email = "nigerian prince sending some gold" #the statement which we want to input
+email = "hey your neighbour jhon here , your house seems to be on fire .this is not a joke" #the statement which we want to input
 
 classifier = Agent(
-    role="email classifier"
-    
+    role = "email classifier",
+    goal = "accurately classify emails based on theri importance. give every email one of these ratings:important, casual or spam",
+    backstory = "You are an AI assistant whose job is to classify mails",
+    verbose = True,
+    allow_delegation = False,
+    llm = model
 )
 
+responder = Agent(
+    role = "email responder",
+    goal = "respond to emails based on their importance. Respond to important emails first then casual emails, and ignore spam emails",
+    backstory = "You are an AI assistant whose job is to respond mails",
+    verbose = True,
+    allow_delegation = False,
+    llm = model
+)
 
+classify_email = Task(
+    description = f"Classify the following email : '{email}'",
+    agent = classifier,
+    expected_output = "One of these three options : 'important', 'casual' or 'spam'",
+)
+respond_to_email = Task(
+    description = f"Respond to email : '{email}' based on the importance provided by the '{classifier} agent.", #ya to ye ya to bas cassifier
+    agent = classifier,
+    expected_output = "A very concise response to email based on importance provided by the '{classifier} agent.",
+)
+crew = Crew(
+    agents= [classifier,responder],
+    tasks= [classify_email,respond_to_email],
+    verbose=2,
+    process=Process.sequential
+)
+output = crew.kickoff()
+print(output)
